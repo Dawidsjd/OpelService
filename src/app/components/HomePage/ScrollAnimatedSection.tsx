8ec352cf7c93bbf8fab89af42a0fface9e66878b
atek <53567837+Dawidsjd@users.noreply.github.com>
2024-09-02 13:18:18 +0200

    adding aniamted background to scrollanimated section

--- src/app/components/HomePage/ScrollAnimatedSection.tsx
+++ src/app/components/HomePage/ScrollAnimatedSection.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ContainerScroll } from '../ui/ContainerScrollAnimation';
+import { WavyBackground } from '../ui/WavyBackground'; // Import WavyBackground
 
 const ScrollAnimatedSection: React.FC = () => {
   return (
-    <div className="relative w-full bg-gray-100 text-center pb-16">
-      
-      <div className="flex flex-col overflow-hidden">
+    <div className="relative w-full text-center pb-16 bg-black min-h-screen shadow-dark">
+      {/* Dodanie WavyBackground jako tło */}
+      <div className="absolute inset-0 z-0 overflow-hidden h-full">
+        <WavyBackground
+          colors={["#38bdf8", "#818cf8", "#c084fc", "#e879f9", "#22d3ee"]}
+          waveWidth={50}
+          speed="fast"
+          waveOpacity={0.3}
+          className="absolute inset-0 h-full"
+        />
+      </div>
+
+      <div className="relative z-10 flex flex-col overflow-hidden h-full  md:-top-44">
         <ContainerScroll
           titleComponent={
             <>
-              <h1 className="text-4xl font-semibold text-black dark:text-white">
-                Unleash the power of <br />
+              <h1 className="text-4xl font-semibold text-white">
+              Professional Services That Deliver! <br />
                 <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                  Scroll Animations
+                Opel Service
                 </span>
               </h1>
             </>
@@ -31,7 +42,7 @@ const ScrollAnimatedSection: React.FC = () => {
         </ContainerScroll>
       </div>
 
-      <div className="flex justify-center -mt-52 mb-8">
+      <div className="relative z-10 flex justify-center -mt-44 md:-mt-96 mb-8">
         <Link
           href="/market"
           className="px-6 py-2 bg-[#26313c] text-white font-semibold rounded-lg shadow-md hover:bg-[#2e3a47] focus:outline-none focus:ring-2 focus:ring-[#26313c] focus:ring-opacity-75 transition"
@@ -39,6 +50,7 @@ const ScrollAnimatedSection: React.FC = () => {
           Go to Market
         </Link>
       </div>
+      
     </div>
   );
 };
