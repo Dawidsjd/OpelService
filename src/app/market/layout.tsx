9fb395e17dfa57f5d2e552e50a27b81ae58a2a57
atek <53567837+Dawidsjd@users.noreply.github.com>
2024-09-16 17:56:10 +0200

    updating metadata

--- src/app/market/layout.tsx
+++ src/app/market/layout.tsx
@@ -1,16 +1,27 @@
+// app/market/layout.tsx
+
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Opel Service | market",
+  description: "Browse our market section for Opel parts and accessories",
+  icons: {
+    icon: "/logo/logo.png", // Ścieżka do ikony
+  },
+};
+
 export default function MarketDetailsLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: ReactNode;
 }) {
-    return (
-        <>
-        <div className="min-h-screen relative bg-[#14181f] bg-grid-white/[0.1]">
-        <div className="absolute inset-0 pointer-events-none flex items-center justify-center bg-[#14181f] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,#14181f)]"></div>
-        <div className="relative z-20">
+  return (
+    <div className="min-h-screen relative bg-[#14181f] bg-grid-white/[0.1]">
+      <div className="absolute inset-0 pointer-events-none flex items-center justify-center bg-[#14181f] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,#14181f)]"></div>
+      <div className="relative z-20">
         {children}
-        </div>
-        </div>
-        </>
-    );
+      </div>
+    </div>
+  );
 }
--- src/app/opel/layout.tsx
+++ src/app/opel/layout.tsx
@@ -1,16 +1,25 @@
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Opel Service | repairs",
+  description: "Explore the repairs section for your Opel vehicle",
+  icons: {
+    icon: "/logo/logo.png", // Ścieżka do ikony
+  },
+};
+
 export default function HomeDetailsLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: ReactNode;
 }) {
-    return (
-        <>
-        <div className="min-h-screen relative bg-[#14181f] bg-grid-white/[0.1]">
-        <div className="absolute inset-0 pointer-events-none flex items-center justify-center bg-[#14181f] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,#14181f)]"></div>
-        <div className="relative z-20">
+  return (
+    <div className="min-h-screen relative bg-[#14181f] bg-grid-white/[0.1]">
+      <div className="absolute inset-0 pointer-events-none flex items-center justify-center bg-[#14181f] [mask-image:radial-gradient(ellipse_at_center,transparent_10%,#14181f)]"></div>
+      <div className="relative z-20">
         {children}
-        </div>
-        </div>
-        </>
-    );
+      </div>
+    </div>
+  );
 }
